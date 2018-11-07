e480d9108546389117e5c2c23d35e78a1f4e7880
Unknown <eli.j.t.jackson@gmail.com>
2018-11-08 12:30:03 +1300

    Main Menu Changes

---
+++ endlessRunner/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class MainMenu : MonoBehaviour
+{
+    public Image canvasText1;
+    void Start()
+    {
+        Invoke("DisableText", 5f);//invoke after 5 seconds
+    }
+    void DisableText()
+    {
+        canvasText1.enabled = false;
+    }
+}
