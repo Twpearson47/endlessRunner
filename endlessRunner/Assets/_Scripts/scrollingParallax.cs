dc57861f2d485a6961d21663f639a07114c73675
Unknown <t.wpearson47@gmail.com>
2018-11-12 12:18:04 +1300

    starting paralax

---
+++ endlessRunner/Assets/_Scripts/scrollingParallax.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollingParallax : MonoBehaviour {
+
+    public float backgroundSize;
+
+    private Transform cameraTransform;
+    private Transform[] layers;
+    private float viewZone = 10;
+    private int leftIndex;
+    private int rightIndex;
+
+
+	// Use this for initialization
+	void Start () {
+
+        cameraTransform = camera.main.transform;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
