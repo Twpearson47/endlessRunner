8345fd269d88774041507fcf7ab1b21ae98942d5
Unknown <eli.j.t.jackson@gmail.com>
2018-11-12 15:07:44 +1300

    Completed Paralax

--- endlessRunner/Assets/Scripts/Movement.cs
+++ endlessRunner/Assets/Scripts/Movement.cs
@@ -5,12 +5,35 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float speed = 0.15f;
+    public float JumpHeight;
+    public bool isGrounded;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        isGrounded = true;
+    }
 
     void Update()
     {
+        if (Input.GetKeyDown("space") && isGrounded)
+        {
+            rb.AddForce(Vector2.up * JumpHeight, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+
         if (Input.GetKey(KeyCode.RightArrow))
         {
             gameObject.transform.position += new Vector3(speed, 0, 0);
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == ("Ground") && isGrounded == false)
+        {
+            isGrounded = true;
         }
     }
 }
