462b1a6b40beeb27c53089a9ddb79478db0b54f4
Jayne Gale <jaynegale@gmail.com>
2018-12-05 20:10:53 +1300

    Main Menu buttons working, including Toggle Audio
    In separate scene, and separate scripts

---
+++ endlessRunner/Assets/Scripts/CheckSoundState.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CheckSoundState : MonoBehaviour
+{
+  //    public GameObject cameraAudio;
+  //  bool audioOn;
+
+    void Start()
+    {
+        GetComponent<AudioListener>().enabled = SoundController.audioState;
+    }
+
+    void Update()
+    {
+        GetComponent<AudioListener>().enabled = SoundController.audioState;
+    }
+
+    public void ToggleAudio()
+    {
+        Debug.Log("AudioState " + SoundController.audioState);
+        SoundController.audioState = !SoundController.audioState;
+        Debug.Log("AudioState " + SoundController.audioState);
+        GetComponent<AudioListener>().enabled = SoundController.audioState;
+    }
+}
